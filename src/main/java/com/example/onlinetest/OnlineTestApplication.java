6097164bff637ca1d62bc181d8abefd53e661007
1notbad19-pixel <1notbad19@gmail.com>
2026-06-02 23:27:17 +0300

    Поправка

--- src/main/java/com/example/onlinetest/OnlineTestApplication.java
+++ src/main/java/com/example/onlinetest/OnlineTestApplication.java
@@ -2,13 +2,28 @@ package com.example.onlinetest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-@EnableAsync
 public class OnlineTestApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(OnlineTestApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                    .allowedOrigins("*")
+                    .allowedMethods("*")
+                    .allowedHeaders("*")
+                    .allowCredentials(false);
+            }
+        };
     }
 }
